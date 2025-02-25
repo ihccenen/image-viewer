3af331844fbff86929ad73dd0d42d93a50cd3549
ihccenen <ihccenen1@gmail.com>
2025-02-25 18:04:43 -0300

    Add image loaded event

--- src/event.zig
+++ src/event.zig
@@ -1,6 +1,8 @@
 const std = @import("std");
+const Image = @import("Image.zig");
 
 pub const Event = union(enum) {
     keyboard: u32,
     resize: struct { usize, usize },
+    image_loaded: usize,
 };
