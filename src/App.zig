645750d40cad829992514e2a4fb9417b9bee86ec
ihccenen <ihccenen1@gmail.com>
2025-03-05 21:05:23 -0300

    Add write and read event and move to app

---
+++ src/App.zig
@@ -0,0 +1,165 @@
+const std = @import("std");
+const Allocator = std.mem.Allocator;
+const Window = @import("Window.zig");
+const Renderer = @import("Renderer.zig");
+const Image = @import("Image.zig");
+const Event = @import("event.zig").Event;
+
+const App = @This();
+
+pub fn loadImage(image: *Image, path: [:0]u8, pipe_fd: std.posix.fd_t, index: usize) void {
+    image.* = Image.init(path) catch return;
+    const event = Event{ .image_loaded = @intCast(index) };
+    _ = std.posix.write(pipe_fd, std.mem.asBytes(&event)) catch return;
+}
+
+window: *Window,
+renderer: *Renderer = undefined,
+paths: [][:0]u8,
+index: usize,
+image: Image = undefined,
+loading_image: bool,
+event: Event = undefined,
+allocator: Allocator,
+
+pub fn init(allocator: Allocator, paths: [][:0]u8) !App {
+    var window = try allocator.create(Window);
+    try window.init(1, 1);
+
+    var renderer = try allocator.create(Renderer);
+    renderer.* = try Renderer.init();
+
+    var image = try Image.init(paths[0]);
+    renderer.setTexture(image);
+    image.deinit();
+
+    return .{
+        .window = window,
+        .renderer = renderer,
+        .paths = paths,
+        .index = 0,
+        .loading_image = false,
+        .allocator = allocator,
+    };
+}
+
+pub fn deinit(self: *App) void {
+    self.window.deinit();
+    self.renderer.deinit();
+    self.allocator.destroy(self.window);
+    self.allocator.destroy(self.renderer);
+}
+
+fn waitEvent(self: *App) void {
+    var pfds = [_]std.posix.pollfd{
+        .{
+            .fd = self.window.wl_display_fd,
+            .events = std.os.linux.POLL.IN,
+            .revents = 0,
+        },
+        .{
+            .fd = self.window.pipe_fds[0],
+            .events = std.os.linux.POLL.IN,
+            .revents = 0,
+        },
+    };
+
+    if (!self.window.wl_display.prepareRead()) {
+        return;
+    }
+
+    _ = self.window.wl_display.flush();
+
+    _ = std.posix.poll(&pfds, -1) catch {
+        self.window.wl_display.cancelRead();
+        return;
+    };
+
+    if (pfds[0].revents != 0 & std.os.linux.POLL.IN) {
+        _ = self.window.wl_display.readEvents();
+    } else {
+        _ = self.window.wl_display.cancelRead();
+    }
+}
+
+fn nextImage(self: *App, step: isize) !void {
+    const next_index = @as(isize, @intCast(self.index)) + step;
+
+    if (!self.loading_image and next_index >= 0 and @as(usize, @intCast(next_index)) < self.paths.len) {
+        self.loading_image = true;
+        var thread = try std.Thread.spawn(.{}, loadImage, .{ &self.image, self.paths[@intCast(next_index)], self.window.pipe_fds[1], @as(usize, @intCast(next_index)) });
+        thread.detach();
+    }
+}
+
+fn keyboardHandler(self: *App, keysym: u32) !void {
+    var buf: [128:0]u8 = undefined;
+    self.window.keyboard.getName(keysym, &buf);
+
+    if (std.mem.orderZ(u8, &buf, "plus") == .eq) {
+        self.renderer.zoom(.in);
+    } else if (std.mem.orderZ(u8, &buf, "minus") == .eq) {
+        self.renderer.zoom(.out);
+    } else if (std.mem.orderZ(u8, &buf, "s") == .eq) {
+        self.renderer.fit(.width);
+    } else if (std.mem.orderZ(u8, &buf, "w") == .eq) {
+        self.renderer.fit(.both);
+    } else if (std.mem.orderZ(u8, &buf, "o") == .eq) {
+        self.renderer.zoom(.none);
+    } else if (std.mem.orderZ(u8, &buf, "k") == .eq) {
+        self.renderer.move(.up);
+    } else if (std.mem.orderZ(u8, &buf, "l") == .eq) {
+        self.renderer.move(.right);
+    } else if (std.mem.orderZ(u8, &buf, "j") == .eq) {
+        self.renderer.move(.down);
+    } else if (std.mem.orderZ(u8, &buf, "h") == .eq) {
+        self.renderer.move(.left);
+    } else if (std.mem.orderZ(u8, &buf, "m") == .eq) {
+        self.renderer.move(.center);
+    } else if (std.mem.orderZ(u8, &buf, "q") == .eq) {
+        self.window.running = false;
+    } else if (std.mem.orderZ(u8, &buf, "n") == .eq) {
+        try self.nextImage(1);
+    } else if (std.mem.orderZ(u8, &buf, "p") == .eq) {
+        try self.nextImage(-1);
+    }
+}
+
+fn readEvents(self: *App) !void {
+    _ = self.window.wl_display.dispatchPending();
+
+    var event: Event = undefined;
+
+    const size = std.posix.read(self.window.pipe_fds[0], std.mem.asBytes(&event)) catch |e| switch (e) {
+        error.WouldBlock => 0,
+        else => unreachable,
+    };
+
+    if (size > 0) {
+        switch (event) {
+            .keyboard => |keysym| try self.keyboardHandler(keysym),
+            .resize => |dim| {
+                const width, const height = dim;
+                self.renderer.viewport(width, height);
+            },
+            .image_loaded => |new_index| {
+                self.loading_image = false;
+                self.index = new_index;
+                self.renderer.setTexture(self.image);
+                self.image.deinit();
+            },
+        }
+    }
+}
+
+pub fn run(self: *App) !void {
+    while (!self.window.shouldClose()) {
+        if (self.renderer.need_redraw) {
+            self.renderer.render();
+            try self.window.swapBuffers();
+        }
+
+        self.waitEvent();
+        try self.readEvents();
+    }
+}
--- src/main.zig
+++ src/main.zig
@@ -1,20 +1,10 @@
 const std = @import("std");
+const Allocator = std.mem.Allocator;
 const Window = @import("Window.zig");
 const Renderer = @import("Renderer.zig");
-const Image = @import("Image.zig");
-const Event = @import("event.zig").Event;
-const Mat4 = @import("math.zig").Mat4;
+const App = @import("App.zig");
 
 const supported_formats = [_][:0]const u8{ ".jpeg", ".jpg", ".png", ".psd" };
-
-pub fn loadImage(image: *Image, paths: [][:0]u8, pipe_fd: std.posix.fd_t, index: usize, step: isize) void {
-    const next_index = @as(isize, @intCast(index)) + step;
-
-    image.* = Image.init(paths[@intCast(next_index)]) catch return;
-
-    const event = Event{ .image_loaded = @intCast(next_index) };
-    _ = std.posix.write(pipe_fd, std.mem.asBytes(&event)) catch return;
-}
 
 pub fn main() !void {
     var gpa = std.heap.GeneralPurposeAllocator(.{}){};
@@ -39,84 +29,7 @@ pub fn main() !void {
         return;
     }
 
-    var index: usize = 0;
-
-    var window = Window{};
-    try window.init(100, 100);
-    defer window.deinit();
-
-    var renderer = try Renderer.init();
-    defer renderer.deinit();
-
-    var image = try Image.init(paths.items[index]);
-    renderer.setTexture(image);
-    image.deinit();
-
-    var loading_image: bool = false;
-    var event: Event = undefined;
-
-    while (!window.shouldClose()) {
-        const size = std.posix.read(window.pipe_fds[0], std.mem.asBytes(&event)) catch |e| switch (e) {
-            error.WouldBlock => 0,
-            else => unreachable,
-        };
-
-        if (size > 0) {
-            switch (event) {
-                .keyboard => |keysym| {
-                    var buf: [128:0]u8 = undefined;
-                    window.keyboard.getName(keysym, &buf);
-
-                    if (std.mem.orderZ(u8, &buf, "plus") == .eq) {
-                        renderer.zoom(.in);
-                    } else if (std.mem.orderZ(u8, &buf, "minus") == .eq) {
-                        renderer.zoom(.out);
-                    } else if (std.mem.orderZ(u8, &buf, "s") == .eq) {
-                        renderer.fit(.width);
-                    } else if (std.mem.orderZ(u8, &buf, "w") == .eq) {
-                        renderer.fit(.both);
-                    } else if (std.mem.orderZ(u8, &buf, "o") == .eq) {
-                        renderer.zoom(.none);
-                    } else if (std.mem.orderZ(u8, &buf, "k") == .eq) {
-                        renderer.move(.up);
-                    } else if (std.mem.orderZ(u8, &buf, "l") == .eq) {
-                        renderer.move(.right);
-                    } else if (std.mem.orderZ(u8, &buf, "j") == .eq) {
-                        renderer.move(.down);
-                    } else if (std.mem.orderZ(u8, &buf, "h") == .eq) {
-                        renderer.move(.left);
-                    } else if (std.mem.orderZ(u8, &buf, "m") == .eq) {
-                        renderer.move(.center);
-                    } else if (std.mem.orderZ(u8, &buf, "q") == .eq) {
-                        window.running = false;
-                    } else if (std.mem.orderZ(u8, &buf, "n") == .eq) {
-                        if (index + 1 < paths.items.len and !loading_image) {
-                            loading_image = true;
-                            var thread = try std.Thread.spawn(.{}, loadImage, .{ &image, paths.items, window.pipe_fds[1], index, 1 });
-                            thread.detach();
-                        }
-                    } else if (std.mem.orderZ(u8, &buf, "p") == .eq) {
-                        if (index > 0 and !loading_image) {
-                            loading_image = true;
-                            var thread = try std.Thread.spawn(.{}, loadImage, .{ &image, paths.items, window.pipe_fds[1], index, -1 });
-                            thread.detach();
-                        }
-                    }
-                },
-                .resize => |dim| {
-                    const width, const height = dim;
-                    renderer.viewport(width, height);
-                },
-                .image_loaded => |new_index| {
-                    loading_image = false;
-                    index = new_index;
-                    renderer.setTexture(image);
-                    image.deinit();
-                },
-            }
-        }
-
-        renderer.render();
-        try window.swapBuffers();
-    }
+    var app = try App.init(allocator, paths.items);
+    defer app.deinit();
+    try app.run();
 }
